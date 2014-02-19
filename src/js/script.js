34cf1105643f4cfb636506e278f6e9643046fcb0
dhulme <>
2014-02-19 11:24:30 +0000

    Added hash detection

--- src/js/script.js
+++ src/js/script.js
@@ -8,14 +8,14 @@ $(function() {
     $(document).foundation();
     attachGlobalEvents();
     
-    processedData = processData(data);
+    var processedData = processData(data);
     
     // Sunburst
     drawSunburst(processedData);
     attachSunburstEvents();
     
     // Bar
-    //drawBar(processedData);
+    drawBar(processedData);
     
   });
 });
@@ -225,7 +225,33 @@ function attachSunburstEvents() {
 }
 
 function drawBar() {
-  
+  var data = [4, 8, 15, 16, 23, 42];
+
+  var width = 420,
+      barHeight = 20;
+
+  var x = d3.scale.linear()
+      .domain([0, d3.max(data)])
+      .range([0, width]);
+
+  var chart = d3.select('#barSvg')
+      .attr("width", width)
+      .attr("height", barHeight * data.length);
+
+  var bar = chart.selectAll("g")
+      .data(data)
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+  bar.append("rect")
+      .attr("width", x)
+      .attr("height", barHeight - 1);
+
+  bar.append("text")
+      .attr("x", function(d) { return x(d) - 3; })
+      .attr("y", barHeight / 2)
+      .attr("dy", ".35em")
+      .text(function(d) { return d; });
 }
 
 function loadDataset(done) {
@@ -297,6 +323,11 @@ function attachGlobalEvents() {
     $('section.content').hide();
     $('#' + self.data('visualisationId')).show();
   });
+  
+  if (window.location.hash) {
+    var hashId = window.location.hash.split('#')[1];
+    $('a[data-visualisation-id="' + hashId + '"]').click();
+  }
 }
 
 // From http://stackoverflow.com/questions/9063383/how-to-invoke-click-event-programmaticaly-in-d3
