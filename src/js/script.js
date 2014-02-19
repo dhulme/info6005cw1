61717f39deea31538037affd7c6888e02c5cd056
dhulme <dhulme@outlook.com>
2014-02-19 13:59:41 +0000

    Bar chart working for cost var in millions

--- src/js/script.js
+++ src/js/script.js
@@ -8,7 +8,7 @@ $(function() {
     $(document).foundation();
     attachGlobalEvents();
     
-    var processedData = processData(data);
+    processedData = processData(data);
     
     // Sunburst
     drawSunburst(processedData);
@@ -63,7 +63,7 @@ function processData(data) {
     if (node.parent) {
       node.parent.projectedActualCostMillions += node.projectedActualCostMillions;
       node.parent.plannedCostMillions += node.plannedCostMillions;
-      node.parent.costVarianceMillions += node.costVarianceMillions;
+      node.parent.costVariancePercentage += node.costVarianceMillions;
     }
   }
   
@@ -224,34 +224,45 @@ function attachSunburstEvents() {
   });
 }
 
-function drawBar() {
-  var data = [4, 8, 15, 16, 23, 42];
-
-  var width = 420,
-      barHeight = 20;
+// Based on http://bost.ocks.org/mike/bar/2/
+function drawBar(data) {
+  var width = $('#barSvg').parents('.svg-container').width()
+    , barHeight = 20;
+    
+  data = data.children;
 
   var x = d3.scale.linear()
-      .domain([0, d3.max(data)])
-      .range([0, width]);
+    .domain([0, d3.max(data, function(d) {
+      return d.costVarianceMillions;
+    })])
+    .range([0, width]);
 
   var chart = d3.select('#barSvg')
-      .attr("width", width)
-      .attr("height", barHeight * data.length);
+    .attr('width', width)
+    .attr('height', barHeight * data.length);
 
-  var bar = chart.selectAll("g")
+  var bar = chart.selectAll('g')
       .data(data)
-    .enter().append("g")
-      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+    .enter().append('g')
+      .attr('transform', function(d, i) {
+        return 'translate(0,' + i * barHeight + ')';
+      });
 
-  bar.append("rect")
-      .attr("width", x)
-      .attr("height", barHeight - 1);
+  bar.append('rect')
+    .attr('width', function(d) {
+      return x(d.costVarianceMillions);
+    })
+    .attr('height', barHeight - 1);
 
-  bar.append("text")
-      .attr("x", function(d) { return x(d) - 3; })
-      .attr("y", barHeight / 2)
-      .attr("dy", ".35em")
-      .text(function(d) { return d; });
+  bar.append('text')
+    .attr('x', function(d) {
+      return x(d.costVarianceMillions) - 3;
+    })
+    .attr('y', barHeight / 2)
+    .attr('dy', '.35em')
+    .text(function(d) { 
+      return d.key; 
+    });
 }
 
 function loadDataset(done) {
